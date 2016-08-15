9e4db1f87c2d934fd0f7fbe858af33abec7c4361
ismailrei <ismailrei@gmail.com>
2016-08-15 11:01:45 +0100

    fix some bugs

--- generators/app/templates/app/scripts/script.js
+++ generators/app/templates/app/scripts/script.js
@@ -2,26 +2,27 @@ let fs = require('fs'),
     path = require('path');
 
 function getDirectories(srcpath) {
-   this.req="devis=require('devis')();";
-	
-   fs.readdirSync(srcpath).filter((file)=>{
-	   if(fs.statSync(path.join(srcpath, file)).isDirectory())
-		   req+="\devis.use('../microservices/"+file+"/main');";
-});
+	this.req = "devis=require('../../devis/devis');";
 
-fs.readFile('app/client.json', 'utf8', (err, data)=>{
-	obj = JSON.parse(data);
-	for(let cl in obj){
-		this.req+="\ndevis.client({type: '"+obj[cl].type+"'\,port: '"+obj[cl].port+"'\,host: '"+obj[cl].host+"'\,protocol: '"+obj[cl].protocol+"'});";	
-	}
-	 this.req+="\nmodule.exports=devis;";  	
-	 fs.writeFile("app/root.js",req, function(err) {
-	   	if(err) {
-	   		return console.log(err);
-    			}
-		console.log("The file was saved!"); 
-  			});	  
+	fs.readdirSync(srcpath).filter((file) => {
+		if (fs.statSync(path.join(srcpath, file)).isDirectory())
+			req += "\ndevis.use('../microservices/" + file + "/main');";
+	});
+
+	fs.readFile('app/client.json', 'utf8', (err, data) => {
+		obj = JSON.parse(data);
+		for (let cl in obj) {
+			this.req += "\ndevis.client({type: '" + obj[cl].type + "'\,port: '" + obj[cl].port + "'\,host: '" + obj[cl].host + "'\,protocol: '" + obj[cl].protocol + "'});";
+		}
+		//this.req+="\nmodule.exports=devis;";  
+		this.req += "\ndevis.listen({host:'127.0.0.1',port:3030});";
+		fs.writeFile("app/root.js", req, function (err) {
+			if (err) {
+				return console.log(err);
+			}
+			console.log("The file was saved!");
+		});
 	});
 }
-	   		
+
 getDirectories('microservices');
