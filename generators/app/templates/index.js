824dc38b5724883891de45bea3b46d97b30d9fd5
ismailrei <ismailrei@gmail.com>
2016-08-15 11:32:18 +0100

    fix some bugs

--- generators/app/templates/index.js
+++ generators/app/templates/index.js
@@ -1,41 +1,24 @@
 //Define variables
-var app=require("express")(),
-    colors=require("colors/safe"),
+var app = require("express")(),
+    colors = require("colors/safe"),
     data = require('./app/wakanda_config'),
     bodyParser = require('body-parser'),
     urlencodedParser = bodyParser.urlencoded({ extended: false }),
-    senecaRoute = require('./app/root');
+    devis = require('../devis/devis');
+devis.client({ host: '127.0.0.1', port: 3030, id: 1 }).setName("index");
 
 var bodyParser = require('body-parser'),
     urlencodedParser = bodyParser.urlencoded({ extended: false }),
-    prefix="/wakandas",
-    func=require("./app/route")({seneca:senecaRoute});
-	
-    //Initialisation of our model
-    senecaRoute.act({role:"model",action:"Initialise",options:data.options[0]});
-    var Data=['ismailrei','182290a+'];
-    var ID=["Test"];
-    senecaRoute.act({role:"auth",action:"login",Data:Data,options:data.options[0]},function(err,sessionID)
-        {   
-            console.log(colors.green(JSON.stringify(sessionID['WASID'])));
-                
-         });
+    prefix = "/wakandas";
+// func=require("./app/route")({devis:devis});
 
-senecaRoute.use('entity');
-app.use(require("body-parser").json());
+//Initialisation of our model
+devis.act({ clientId: 1, role: "model", action: "Initialise", options: data.options[0]},(data)=>{console.log(data);});
+var Data = ['ismailrei', '182290a+'];
+var ID = ["Test"];
+devis.act({ clientId: 1, role: "auth", action: "login", Data: Data, options: data.options[0] }, function (err, sessionID) {
+    console.log(colors.green(JSON.stringify(sessionID)));
 
-app.get(prefix+'/:table/:id',func.GET);
+});
 
-app.delete(prefix+'/:table/:id',func.DELETE);    
 
-app.put(prefix+'/:table', urlencodedParser,func.PUT);
-
-app.post(prefix+'/:table', urlencodedParser,func.POST);
-
-app.use(senecaRoute.export('web'));
-
-app.listen({
-        type: 'http',
-        port: '8888',
-        host: '127.0.0.1',
-        protocol: 'http'});
