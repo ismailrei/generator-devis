8b2f905b3eb0e91dc29ed31737a87da061cb9d78
Ismail Reida <ismailrei@gmail.com>
2016-08-19 12:08:04 +0100

    use ecma6

--- generators/app/templates/index.js
+++ generators/app/templates/index.js
@@ -1,40 +1,44 @@
 //Define variables
-var app = require("express")(),
+let app = require("express")(),
     colors = require("colors/safe"),
-    data = require('./app/wakanda_config'),
-    bodyParser = require('body-parser'),
+    data = require('./app/wakanda_config');
+
+let bodyParser = require('body-parser'),
     urlencodedParser = bodyParser.urlencoded({ extended: false }),
-    devis = require('../devis/devis');
+    devis = require('../devis/devis'),
+    prefix = "/wakandas";
+
 devis.client({ host: '127.0.0.1', port: 3030, id: 1 }).setName("index");
 
-var bodyParser = require('body-parser'),
-    urlencodedParser = bodyParser.urlencoded({ extended: false }),
-    prefix = "/wakandas";
-func=require("./app/route")({devis:devis});
+
+func = require("./app/route")({ devis: devis });
 
 //Initialisation of our model
-devis.act({ clientId: 1, role: "model", action: "Initialise", options: data.options[0]},(data)=>{});
-var Data = ['ismailrei', '182290a+'];
-var ID = ["Test"];
+devis.act({ clientId: 1, role: "model", action: "Initialise", options: data.options[0] }, ()=>{});
+
+let Data = ['ismailrei', '182290a+'];
+let ID = ["Test"];
 devis.act({ clientId: 1, role: "auth", action: "login", Data: Data, options: data.options[0] }, function (err, sessionID) {
     console.log(colors.green(JSON.stringify(sessionID)));
 
 });
+
 app.use(require("body-parser").json());
 
-app.get(prefix+'/:table/:id',func.GET);
+app.get(prefix + '/:table/:id', func.GET);
 
-app.delete(prefix+'/:table/:id',func.DELETE);    
+app.delete(prefix + '/:table/:id', func.DELETE);
 
-app.put(prefix+'/:table', urlencodedParser,func.PUT);
+app.put(prefix + '/:table', urlencodedParser, func.PUT);
 
-app.post(prefix+'/:table', urlencodedParser,func.POST);
+app.post(prefix + '/:table', urlencodedParser, func.POST);
 
 
 app.listen({
-        type: 'http',
-        port: '8888',
-        host: '127.0.0.1',
-        protocol: 'http'});
+    type: 'http',
+    port: '8888',
+    host: '127.0.0.1',
+    protocol: 'http'
+});
 
 
